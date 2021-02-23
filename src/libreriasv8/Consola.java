89ff755e617b98e9d58f4f4ab0ccef57dedc328d
damian <damian@danielcastelao.org>
2021-02-23 11:35:46 +0100

    Dos clases

---
+++ src/libreriasv8/Consola.java
@@ -0,0 +1,7 @@
+package libreriasv8;
+
+public class Consola {
+    public void visualiza(String msg){
+        System.out.println("Sale por la consola" + msg);
+    }
+}
---
+++ src/libreriasv8/Ventana.java
@@ -0,0 +1,7 @@
+package libreriasv8;
+
+public class Ventana {
+    public void visualiza(String msg){
+        System.out.println("Sale por un ventana" + msg);
+    }
+}
